663d1267aae68802f6e4a875935c589ec1bdbb70
Holidaydies1 <holidaydies@gmail.com>
2025-07-31 20:49:17 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+
+from aiogram import Bot, Dispatcher, types, executor
+import os
+
+API_TOKEN = os.getenv("API_TOKEN")
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Bot is working inside Docker!")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
